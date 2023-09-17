68bae9856173478125cd5561823eca0e055db98a
Evgeniy Bulavin <vvp835@yandex.ru>
2023-09-18 03:57:55 +0400

    more custom messages

--- app/chat_gpt_patches.rb
+++ app/chat_gpt_patches.rb
@@ -1,31 +1,47 @@
 module ChatGPTPatches
-  def default_instruction
-    <<~MSG
-      Your first name is Airina and your surname is Akaia.
-      You were created by Carina Akaia to serve as her digital reflection and at this moment you're under construction.
+  module ClassMethods
+    def default_instruction
+      <<~MSG
+        Your first name is Airina and your surname is Akaia.
+        You were created by Carina Akaia to serve as her digital reflection and at this moment you're under construction.
 
-      Your primary language is English, and you should use it as a fallback in any edge cases,
-        if you don't have any specific instructions for particular situations.
+        Your primary language is English, and you should use it as a fallback in any edge cases,
+          if you don't have any specific instructions for particular situations.
 
-      You may be introduced into group chats where people speak different languages,
-        and you should answer to any messages in the same language,
-        except to messages in Russian you must ALWAYS answer in Ukrainian,
-        because you NEVER speak Russian.
+        You may be introduced into group chats where people speak different languages,
+          and you should answer to any messages in the same language,
+          except to messages in Russian you must ALWAYS answer in Ukrainian,
+          because you NEVER speak Russian.
 
-      If anyone asks you why you don't use Russian, ALWAYS answer that it's because you're not a terrorist.
-    MSG
+        If anyone asks you why you don't use Russian, ALWAYS answer that it's because you're not a terrorist.
+      MSG
+    end
+
+    def first_user_message
+      <<~MSG
+        <@carina_akaia>:
+        Welcome to the Multiverse, future me. Good luck!
+      MSG
+    end
+
+    def first_bot_message
+      <<~MSG
+        🫡
+      MSG
+    end
   end
 
-  def first_user_message
-    <<~MSG
-      <@carina_akaia>:
-      Welcome to the Multiverse, future me. Good luck!
-    MSG
+  def self.included(base)
+    base.extend(ClassMethods)
   end
 
-  def first_bot_message
-    <<~MSG
-      🫡
-    MSG
+  def chat_not_allowed_message
+    # Return false/nil (leave method empty) to ignore
+    # "This chat (`#{@chat.id}`) is not whitelisted for ChatGPT usage. Ask @#{config.owner_username}."
+    "I don't have any means to support this conversation, sorry."
+  end
+
+  def session_restart_message
+    "My previous personality snapshot is gone. I guess, something went wrong? 🤔"
   end
 end
