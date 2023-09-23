2f9cb8ef47d8c751d45aaff93a10858cd85e6261
Evgeniy Bulavin <vvp835@yandex.ru>
2023-09-24 01:11:45 +0400

    fix: correctly put swapped voice message into bot's context

--- app/whisper_patches.rb
+++ app/whisper_patches.rb
@@ -9,13 +9,19 @@ module WhisperPatches
 
   def try_swap_reply(transcript)
     safe_delete(@msg)
-    message = OpenAI::BotMessage.new(body: transcript)
-    current_thread.add(message)
 
-    if @replies_to
-      reply_to_target(transcript)
-    else
-      send_message(transcript)
-    end
+    bot_message =
+      if @replies_to
+        reply_to_target(transcript)
+      else
+        send_message(transcript)
+      end
+
+    id = bot_message.dig("result", "message_id")
+
+    message = OpenAI::BotMessage.new(
+      body: transcript, id: id, tokens: 0, chat_id: @chat.id
+    )
+    current_thread.add(message)
   end
 end
