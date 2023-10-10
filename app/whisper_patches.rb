a3597e66993f404ed468ef6e1f13554c0cb953fb
Carina.Akaia.near <carina@dmail.ai>
2023-10-10 11:38:00 +0400

    wip

--- app/whisper_patches.rb
+++ app/whisper_patches.rb
@@ -14,14 +14,18 @@ module WhisperPatches
       if @replies_to
         reply_to_target(transcript)
       else
-        send_message(transcript)
+        reply(transcript)
       end
 
     id = bot_message.dig("result", "message_id")
 
     message = OpenAI::BotMessage.new(
-      body: transcript, id: id, tokens: 0, chat_id: @chat.id
+      id: id,
+      tokens: 0,
+      chat_id: @chat.id,
+      body: transcript,
     )
+
     current_thread.add(message)
   end
 end
